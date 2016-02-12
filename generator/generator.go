b087d54ff9d6860c1a559d9f87421d88a2b178f9
alesr <alessandro.resta@gmail.com>
2016-02-12 17:59:24 +0000

    added functions to generate fake arithmetic and fibonacci operations

--- generator/generator.go
+++ generator/generator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -12,8 +13,8 @@ var (
 	errRandomFuncError = errors.New("random: function expect an int arg greater than zero")
 )
 
-// Messages randomly creates messages to be sent to our dear octopus so he can
-// distribute them to the right hands.
+// Messages randomly creates elements to be sent to our dear octopus,
+// so he can distribute them to the right hands.
 func Messages() {
 
 	// the basic operations that the octopus are prepared to handle.
@@ -26,26 +27,53 @@ func Messages() {
 		log.Fatal(err)
 	}
 
+	var msg []string
 	switch taskList[index] {
 	case "arithmetic":
-
+		msg, err = arithmetic()
+		if err != nil {
+			log.Fatal(err)
+		}
 	case "fibonacci":
+		msg = fibonacci()
 	case "reverse":
 	case "encode":
 	}
 
-	fmt.Println()
-
+	fmt.Println(msg)
 }
 
 // returns a pseudo random number between zero and max
 func random(max int) (int, error) {
-
 	// it means that the source slice is empty.
 	if max < 0 {
 		return 0, errRandomFuncError
 	}
 
-	rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().UTC().UnixNano())
 	return rand.Intn(max), nil
+}
+
+// builds a fake arithmetic operation
+func arithmetic() ([]string, error) {
+
+	// let's take a random value for a and b
+	maxValue := 100
+	a, _ := random(maxValue)
+	b, _ := random(maxValue)
+
+	// now a random operation
+	operationsList := []string{"add", "sub", "mult", "div"}
+	index, err := random(len(operationsList))
+	if err != nil {
+		return nil, errRandomFuncError
+	}
+	operation := operationsList[index]
+	return []string{operation, strconv.Itoa(a), strconv.Itoa(b)}, nil
+}
+
+// a fibonacci operation
+func fibonacci() []string {
+	n, _ := random(50)
+	return []string{"fibonacci", strconv.Itoa(n)}
 }
