93e23cd0eaff7e62b724accedb5726cbf6026a22
alesr <alessandro.resta@gmail.com>
2016-02-13 17:47:36 +0000

    cleaning main func

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/alesr/octopus-distributor/assembler"
-)
+import "github.com/alesr/octopus-distributor/distributor"
 
 func main() {
-
-	requestCh := make(chan []string)
-	doneCh := make(chan bool)
-	go assembler.GetRequest(requestCh, doneCh)
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-requestCh)
-	}
-	doneCh <- true
-	close(doneCh)
+	distributor.Receiver()
 }
