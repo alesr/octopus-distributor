2cab18ee43d43f557205cdb1f5bc4741fec54d8f
alesr <alessandro.resta@gmail.com>
2016-02-14 12:33:03 +0000

    added register func to include an ID to request and identify task

--- distributor/distributor.go
+++ distributor/distributor.go
@@ -1,19 +1,66 @@
 package distributor
 
 import (
-	"fmt"
-	"strconv"
+	"log"
 
 	"github.com/alesr/octopus-distributor/assembler"
 )
 
-// Receiver triggers the system to start receive requests and steam the machine.
-func Receiver() {
+// Arithmetic ...
+type Arithmetic struct {
+	request          []string
+	operation        string
+	id, a, b, result int
+}
+
+// Fibonacci ...
+type Fibonacci struct {
+	request       []string
+	id, n, result int
+}
+
+// Reverse ...
+type Reverse struct {
+	request      []string
+	id           int
+	text, result string
+}
+
+// Encode ..
+type Encode struct {
+	request      []string
+	id           int
+	text, result string
+}
+
+// Parser ..
+type Parser interface {
+	parse() error
+}
+
+func (a *Arithmetic) parse(request map[string]string) error {
+	return nil
+}
+
+func (f *Fibonacci) parse(request map[string]string) error {
+	return nil
+}
+
+func (r *Reverse) parse(request map[string]string) error {
+	return nil
+}
+
+func (e *Encode) parse(request map[string]string) error {
+	return nil
+}
+
+// Receive triggers the system to start receive requests and steam the machine.
+func Receive() {
 	requestCh := make(chan []string)
 	go assembler.GetRequest(requestCh)
 
 	for i := 0; i < 10; i++ {
-		identifier(i, <-requestCh)
+		register(i, <-requestCh)
 	}
 }
 
@@ -28,22 +75,29 @@ func Receiver() {
 // in a map to make it easy to handle later.
 // we'll organize the received request under the form:
 // [id: XX, op: encode, argX: some string]
-func identifier(id int, req []string) {
+func register(i int, request []string) {
 
-	request := make(map[string]string)
+	var task interface{}
 
-	request["id"] = strconv.Itoa(id)
-	request["op"] = req[0]
+	switch request[0] {
+	case "add":
+		task = &Arithmetic{request: request, id: i}
+	case "sub":
+		task = &Arithmetic{request: request, id: i}
+	case "mult":
+		task = &Arithmetic{request: request, id: i}
+	case "div":
+		task = &Arithmetic{request: request, id: i}
+	case "fibonacci":
+		task = &Fibonacci{request: request, id: i}
+	case "reverse":
+		task = &Reverse{request: request, id: i}
+	case "enconde":
+		task = &Encode{request: request, id: i}
+	default:
 
-	// no mater how many args, we handle it!
-	for i, arg := range req[1:] {
-		key := fmt.Sprintf("arg %s", strconv.Itoa(i+1))
-		request[key] = arg
+		// REVIEW: to work on data later
+		log.Fatal("invalid request")
 	}
-	dealer(request)
-}
-
-// now, depending on the request type we'll distribute it to the right function
-func dealer(request map[string]string) {
 
 }
---
+++ solutioner/fibonacci.go
@@ -0,0 +1,5 @@
+package solutioner
+
+func fibonacci(c chan int)) {
+
+}
